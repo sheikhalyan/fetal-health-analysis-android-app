f36a5f83fe18b8433b77890b5ecf84caf8def363
sheikhalyan <alyantaj789l@gmail.com>
2026-06-30 16:59:09 +0500

    Remove .idea and original FYP_QUERY.sql from tracking

--- FYP_QUERY.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE fetalhealthanalysis;
-USE fetalhealthanalysis;
-
-CREATE TABLE RegisteredUsers (
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    phone VARCHAR(20)
-);
